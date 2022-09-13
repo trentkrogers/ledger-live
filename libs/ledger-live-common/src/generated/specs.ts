29a454dc0a2b7a3e9436370559e5198e93e58c15
Trent Rogers <trentkrogers@gmail.com>
2022-09-13 13:50:08 -0500

    C chain WIP

--- libs/ledger-live-common/src/generated/specs.ts
+++ libs/ledger-live-common/src/generated/specs.ts
@@ -1,4 +1,5 @@
 import algorand from "../families/algorand/specs";
+import avalanchecchain from "../families/avalanchecchain/specs";
 import bitcoin from "../families/bitcoin/specs";
 import cardano from "../families/cardano/specs";
 import celo from "../families/celo/specs";
@@ -19,6 +20,7 @@ import tron from "../families/tron/specs";
 
 export default {
   algorand,
+  avalanchecchain,
   bitcoin,
   cardano,
   celo,
